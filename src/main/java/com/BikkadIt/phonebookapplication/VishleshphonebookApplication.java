87a727aa486f94e6c0cd5328bb996f31c727b24a
vishlesh11996 <vishlesh.reakalwar@gmail.com>
2022-06-19 16:17:48 +0530

    :PHN-1:create git hub repo

---
+++ src/main/java/com/BikkadIt/phonebookapplication/VishleshphonebookApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.phonebookapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishleshphonebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VishleshphonebookApplication.class, args);
+	}
+
+}
